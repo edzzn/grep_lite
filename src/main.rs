921014eccddce99fea51d230e2fff18e94fa0a53
Edisson Reinozo <edisson350@gmail.com>
2023-11-12 10:58:04 -0500

    Add input file and error handling to main function

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,46 @@
 use clap::{Arg, Command};
 use regex::Regex;
+use std::error::Error;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let matches = Command::new("gep-lite")
         .version("1.0")
         .arg(
             Arg::new("pattern")
                 .short('p')
                 .long("pattern")
-                .help("Patter to search"),
+                .help("Patter to search")
+                .required(true),
+        )
+        .arg(
+            Arg::new("input")
+                .short('i')
+                .long("input")
+                .help("File to search")
+                .required(true),
         )
         .get_matches();
 
     let pattern = matches
         .get_one::<String>("pattern")
         .expect("Missing pattern");
+    let re = Regex::new(pattern)?;
 
-    let re = Regex::new(pattern).unwrap();
-    let text = "\
-    Every face, every shop,
-    bedroom window, public-house, and
-    dark square is a picture
-    feverishly turned--in search of what?
-    It is the same with books.
-    What do we seek
-    through millions of pages?";
+    let file = matches
+        .get_one::<String>("input")
+        .expect("Missing input file");
+    let f = File::open(file)?;
+    let reader = BufReader::new(f);
 
-    for (i, line) in text.lines().enumerate() {
-        match re.find(line) {
-            Some(_) => println!("{}: {}", i, line),
-            _ => (),
+    for (i, line) in reader.lines().enumerate() {
+        let line = line?;
+        let line_num = i + 1;
+        if let Some(_) = re.find(&line) {
+            println!("{}: {}", line_num, line);
         }
     }
+
+    Ok(())
 }
