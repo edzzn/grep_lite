e0f3b21852ed3eddf13ab7939bb56f6eebe48d2d
Edisson Reinozo <edisson350@gmail.com>
2023-11-12 10:38:44 -0500

    Add clap and regex dependencies, update main.rs to search for a pattern

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,35 @@
+use clap::{Arg, Command};
+use regex::Regex;
+
 fn main() {
-    let ctx_lines = 1;
-    let search_term = "e";
+    let matches = Command::new("gep-lite")
+        .version("1.0")
+        .arg(
+            Arg::new("pattern")
+                .short('p')
+                .long("pattern")
+                .help("Patter to search"),
+        )
+        .get_matches();
+
+    let pattern = matches
+        .get_one::<String>("pattern")
+        .expect("Missing pattern");
+
+    let re = Regex::new(pattern).unwrap();
     let text = "\
-        Every face, every shop,
-        bedroom window, public-house, and
-        dark square is a picture
-        feverishly turned--in search of what?
-        It is the same with books.
-        What do we seek
-        through millions of pages?";
-
-    let mut tags: Vec<usize> = vec![];
-    let mut ctx: Vec<Vec<(usize, String)>> = vec![];
+    Every face, every shop,
+    bedroom window, public-house, and
+    dark square is a picture
+    feverishly turned--in search of what?
+    It is the same with books.
+    What do we seek
+    through millions of pages?";
 
     for (i, line) in text.lines().enumerate() {
-        if line.contains(search_term) {
-            tags.push(i);
-            let v = Vec::with_capacity(2 * ctx_lines + 1);
-            ctx.push(v)
-        }
-    }
-
-    if tags.is_empty() {
-        return;
-    }
-
-    for (i, line) in text.lines().enumerate() {
-        for (j, tag) in tags.iter().enumerate() {
-            let lower_bound = tag.saturating_sub(ctx_lines);
-            let upper_bound = tag + ctx_lines;
-
-            if (i >= lower_bound) && (i <= upper_bound) {
-                let line_as_string = String::from(line);
-                let local_ctx = (i, line_as_string);
-                ctx[j].push(local_ctx);
-            }
-        }
-    }
-
-    for local_ctx in ctx.iter() {
-        for &(i, ref line) in local_ctx.iter() {
-            let line_num = i + 1;
-            println!("{}: {}", line_num, line);
+        match re.find(line) {
+            Some(_) => println!("{}: {}", i, line),
+            _ => (),
         }
     }
 }
